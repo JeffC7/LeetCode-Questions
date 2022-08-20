763f885c1ddda4afb1badb5ba4aef094307027ff
JeffC7 <89879614+JeffC7@users.noreply.github.com>
2022-08-20 16:39:40 -0400

    Time: 0 ms (100.00%), Space: 40.8 MB (86.59%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,15 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        boolean wordStart = false;
         int count = 0;
         for (int i = s.length()-1; i >= 0; i--) {
-            if (s.charAt(i) == '\s' && wordStart) {
-                return count;
-            } else if (s.charAt(i) == '\s') {
-                continue;
-            } else {
+            if (s.charAt(i) != ' ') {
                 count++;
-                wordStart = true;
+            } else {
+                if (count > 0) {return count;}
             }
         }
         return count;
